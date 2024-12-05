6e5d181f07a6d2c5c332bc945429624b9af70350
Zoying <144758481+Zoying@users.noreply.github.com>
2024-12-05 16:31:01 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,14 @@
+//New Playground
+int main() {
+    int a, b;
+    vector <int> A, B;
+    while (cin >> a >> b)
+    {
+        A.push_back(a);
+        B.push_back(b);
+    }
+    for (int i = 0; i < A.size(); i ++)
+    {
+        cout << A[i] << " ";
+    }
+}
---
+++ week13/week13-4.cpp
@@ -0,0 +1,19 @@
+//New Playground
+int main() {
+    int a, b;
+    vector <int> A, B;
+    while (cin >> a >> b)
+    {
+        A.push_back(a);
+        B.push_back(b);
+    }
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int ans = 0;
+    for (int i = 0; i < A.size(); i ++)
+    {
+        cout << A[i] << " ";
+        ans += abs(A[i] - B[i]);
+    }
+    cout << "µª®×¬O: " << ans;
+}
